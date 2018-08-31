63d0f93cefb95fdc8919cdfc7798203fb8bf01d7
Ari Chivukula <ari@chivuku.la>
2018-08-31 12:26:50 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,29 @@ var project = ts.createProject("tsconfig.json");
 
 gulp.task(
   "build:1",
-  () => gulp.src("static/index.ts")
+  () => gulp.src("source/index.ts")
     .pipe(replace("FOIA_DB", fs.readFileSync(".foia-db.json", "ascii")))
     .pipe(project())
     .js
-    .pipe(gulp.dest("static/")),
+    .pipe(gulp.dest("build/")),
 );
 
 gulp.task(
   "build:2",
-  () => gulp.src("static/index.js")
+  () => gulp.src("build/index.js")
     .pipe(babel({
       presets: ["@babel/preset-env"],
     }))
-    .pipe(gulp.dest("static/")),
+    .pipe(gulp.dest("build/")),
 );
 
 gulp.task(
   "build:3",
-  () => gulp.src("static/index.js")
+  () => gulp.src("build/index.js")
     .pipe(bro({
       transform: [["uglifyify", { global: true }]],
     }))
-    .pipe(gulp.dest("static/")),
+    .pipe(gulp.dest("build/")),
 );
 
 gulp.task(
