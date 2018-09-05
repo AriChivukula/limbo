40663ab0360a0e246754ae73c6393e3377d2cbe8
Ari Chivukula <ari@chivuku.la>
2018-09-05 12:11:26 -0700

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -15,6 +15,23 @@ import lambda from "aws-serverless-express";
 import express from "express";
 import Rollbar from "rollbar";
 
+export function makeSync<T>(
+  wasAsync: Promise<T>,
+): T {
+  let result = wasAsync
+    .catch((err: Error): void => {
+      console.log(err);
+    })
+    .then((): void => {
+      return;
+    })
+    .catch((err: Error): void => {
+      console.log(err);
+    });
+
+  return result;
+}
+
 const app: express.Express = express();
 const eventAdapter: SlackEventAdapter = createEventAdapter(process.env.TF_VAR_SLACK_SIGNING_SECRET);
 const messageAdapter: SlackMessageAdapter = createMessageAdapter(process.env.TF_VAR_SLACK_SIGNING_SECRET);
@@ -30,15 +47,17 @@ const web: WebClient = new WebClient(
 app.use("/slack/event", eventAdapter.expressMiddleware());
 app.use("/slack/message", messageAdapter.expressMiddleware());
 
-eventAdapter.on("message", async (message: any, body: any): Promise<void> => {
+eventAdapter.on("message", (message: any, body: any): void => {
   if (message.channel_type !== "channel" || message.subtype) {
     return;
   }
-  await web.chat.postMessage({
-    channel: message.channel,
-    text: `Hello <@${message.user}>! :tada:`,
-    thread_ts: message.ts,
-  });
+  makeSync(
+    web.chat.postMessage({
+      channel: message.channel,
+      text: `Hello <@${message.user}>! :tada:`,
+      thread_ts: message.ts,
+    }),
+  );
   return;
 });
 
