5c0a139ff9b2d001c26aa6f59bbea760499b9dcb
Ari Chivukula <ari@chivuku.la>
2019-01-15 11:55:40 -0800

    Update background.ts

--- source/background.ts
+++ source/background.ts
@@ -6,7 +6,7 @@ import {
 } from "./content";
 
 chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
-  if (shouldRefreshPage(tab.url)) {
+  if (shouldRefreshPage(tab.url || "")) {
     chrome.pageAction.show(tabId);
   } else {
     chrome.pageAction.hide(tabId);
