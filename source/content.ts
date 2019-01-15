96e403d88e95b2f2d8749eb6ab92f321eafda68a
Ari Chivukula <ari@chivuku.la>
2019-01-15 13:10:47 -0800

    Update content.ts

--- source/content.ts
+++ source/content.ts
@@ -3,3 +3,12 @@ import "@babel/polyfill";
 export function shouldRefreshPage(url: string): boolean {
   return url.match(/^https:\/\/([a-z0-9]+[.])*westlaw.com\/.*$/) !== null;
 }
+
+setTimeout(
+  () => {
+    if (shouldRefreshPage(window.location.href)) {
+      window.location.reload();
+    }
+  },
+  60 * 60 * 1000,
+);
