59f3ae3fcabc3c0034119f8f8681c882a5bc7e2f
Ari Chivukula <ari@chivuku.la>
2019-05-07 17:23:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -253,7 +253,7 @@ resource "aws_ecs_service" "SERVICE" {
   health_check_grace_period_seconds  = 600
 
   network_configuration {
-    subnets = ["${data.aws_subnet_ids.PUBLIC_SUBNETS.ids}"]
+    subnets = ["${data.aws_subnet_ids.PRIVATE_SUBNETS.ids}"]
     security_groups = ["${aws_security_group.SECURITY.id}"]
   }
 
