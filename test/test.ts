85e033a4ba31bb1ea4442046874e1e1f1f1e7f4c
Ari Chivukula <ari@chivuku.la>
2019-05-19 16:33:39 -0700

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -15,25 +15,54 @@ import {
   shouldRefreshPage,
 } from "../source/content_westlaw";
 
-it(
+describe(
   "shouldRefreshPage",
   async (): Promise<void> => {
-    chai.expect(shouldRefreshPage("https://westlaw.com/")).to.be.true;
-    chai.expect(shouldRefreshPage("https://next.westlaw.com/blah/")).to.be.true;
-    chai.expect(shouldRefreshPage("https://worstlaw.com/")).to.be.false;
+    it(
+      "node",
+      async (): Promise<void> => {
+        chai.expect(shouldRefreshPage("https://westlaw.com/")).to.be.true;
+        chai.expect(shouldRefreshPage("https://next.westlaw.com/blah/")).to.be.true;
+        chai.expect(shouldRefreshPage("https://worstlaw.com/")).to.be.false;
+      },
+    );
+    it(
+      "chrome",
+      async (): Promise<void> => {
+        const browser = await launch({ args: [ "--no-sandbox" ] });
+        const page = await browser.newPage();
+        await page.goto(`file:${join(__dirname, "test.html")}`);
+        const script = readFileSync("build/content_westlaw.js");
+        const ctx = await page.mainFrame().executionContext();
+        const result = await ctx.evaluateHandle(script.toString());
+        chai.expect(JSON.stringify(result.jsonValue())).to.equal("{}");
+        await browser.close();
+      },
+    );
   },
 );
 
-it(
-  "extensionCanLoad",
+describe(
+  "findAndPrintCitations",
   async (): Promise<void> => {
-    const browser = await launch({ args: [ "--no-sandbox" ] });
-    const page = await browser.newPage();
-    await page.goto(`file:${join(__dirname, "test.html")}`);
-    const script = readFileSync("build/content_westlaw.js");
-    const ctx = await page.mainFrame().executionContext();
-    const result = await ctx.evaluateHandle(script.toString());
-    chai.expect(JSON.stringify(result.jsonValue())).to.equal("{}");
-    await browser.close();
+    it(
+      "node",
+      async (): Promise<void> => {
+        chai.expect(findAndPrintCitations("")).to.equal("");
+      },
+    );
+    it(
+      "chrome",
+      async (): Promise<void> => {
+        const browser = await launch({ args: [ "--no-sandbox" ] });
+        const page = await browser.newPage();
+        await page.goto(`file:${join(__dirname, "test.html")}`);
+        const script = readFileSync("build/content_citation.js");
+        const ctx = await page.mainFrame().executionContext();
+        const result = await ctx.evaluateHandle(script.toString());
+        chai.expect(JSON.stringify(result.jsonValue())).to.equal("");
+        await browser.close();
+      },
+    );
   },
 );
