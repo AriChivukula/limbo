1f96c6ef03cf506fa20af049a5388354b2c95253
Ari Chivukula <ari@chivuku.la>
2019-05-19 19:29:48 -0700

    Update content_citation.ts

--- source/content_citation.ts
+++ source/content_citation.ts
@@ -3,13 +3,11 @@ import {
   sitation,
 } from "sitation";
 
-export function findCitations(): string {
-  // @ts-ignore
-  return parseCitations(document.body.innerText);
-}
-
 export function parseCitations(content: string): string {
   return sitation(content);
 }
 
-console.log(findCitations());
+if (typeof document !== 'undefined') {
+  // @ts-ignore
+  console.log(parseCitations(document.body.innerText));
+}
